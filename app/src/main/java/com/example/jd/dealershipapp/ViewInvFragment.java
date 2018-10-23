a99f523c71851d02d312f50a33628978520ca213
James DiNovo <james.dinovo01@stclairconnect.ca>
2018-10-22 22:39:32 -0400

    Fixed Fragments #3 - Fragments now will not add multiple of the same to backstack

--- app/src/main/java/com/example/jd/dealershipapp/ViewInvFragment.java
+++ app/src/main/java/com/example/jd/dealershipapp/ViewInvFragment.java
@@ -12,12 +12,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link SearchInvFragment.OnFragmentInteractionListener} interface
+ * {@link ViewInvFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link SearchInvFragment#newInstance} factory method to
+ * Use the {@link ViewInvFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SearchInvFragment extends Fragment {
+public class ViewInvFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +29,7 @@ public class SearchInvFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public SearchInvFragment() {
+    public ViewInvFragment() {
         // Required empty public constructor
     }
 
@@ -39,11 +39,11 @@ public class SearchInvFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment SearchInvFragment.
+     * @return A new instance of fragment ViewInvFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static SearchInvFragment newInstance(String param1, String param2) {
-        SearchInvFragment fragment = new SearchInvFragment();
+    public static ViewInvFragment newInstance(String param1, String param2) {
+        ViewInvFragment fragment = new ViewInvFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
