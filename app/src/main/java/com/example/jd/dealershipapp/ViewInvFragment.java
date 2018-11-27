e06ba35fb83264c6309ba283f6ef4ef4fbbbfaff
James DiNovo <james.dinovo01@stclairconnect.ca>
2018-11-27 08:48:59 -0500

    Added images - All cars in inventory now have appropriate images

--- app/src/main/java/com/example/jd/dealershipapp/ViewInvFragment.java
+++ app/src/main/java/com/example/jd/dealershipapp/ViewInvFragment.java
@@ -84,12 +84,20 @@ public class ViewInvFragment extends Fragment {
         images.add(R.raw.jeep3);
         vehicleList.add(new Vehicle(getResources().getString(R.string.brand1), getResources().getString(R.string.model1), getResources().getString(R.string.year1), getResources().getString(R.string.price1), getResources().getString(R.string.description1), R.raw.jeep1, images));
         images = new ArrayList<>();
-        images.add(R.raw.wheeler_dealer);
-        images.add(R.raw.wheeler_dealer);
-        images.add(R.raw.wheeler_dealer);
-        vehicleList.add(new Vehicle(getResources().getString(R.string.brand2), getResources().getString(R.string.model2), getResources().getString(R.string.year2), getResources().getString(R.string.price2), getResources().getString(R.string.description2), R.raw.wheeler_dealer, images));
-        vehicleList.add(new Vehicle(getResources().getString(R.string.brand3), getResources().getString(R.string.model3), getResources().getString(R.string.year3), getResources().getString(R.string.price3), getResources().getString(R.string.description3), R.raw.wheeler_dealer, images));
-        vehicleList.add(new Vehicle(getResources().getString(R.string.brand4), getResources().getString(R.string.model4), getResources().getString(R.string.year4), getResources().getString(R.string.price4), getResources().getString(R.string.description4), R.raw.wheeler_dealer, images));
+        images.add(R.raw.mustang1);
+        images.add(R.raw.mustang2);
+        images.add(R.raw.mustang3);
+        vehicleList.add(new Vehicle(getResources().getString(R.string.brand2), getResources().getString(R.string.model2), getResources().getString(R.string.year2), getResources().getString(R.string.price2), getResources().getString(R.string.description2), R.raw.mustang1, images));
+        images = new ArrayList<>();
+        images.add(R.raw.gmc1);
+        images.add(R.raw.gmc2);
+        images.add(R.raw.gmc3);
+        vehicleList.add(new Vehicle(getResources().getString(R.string.brand3), getResources().getString(R.string.model3), getResources().getString(R.string.year3), getResources().getString(R.string.price3), getResources().getString(R.string.description3), R.raw.gmc1, images));
+        images = new ArrayList<>();
+        images.add(R.raw.mini1);
+        images.add(R.raw.mini2);
+        images.add(R.raw.mini3);
+        vehicleList.add(new Vehicle(getResources().getString(R.string.brand4), getResources().getString(R.string.model4), getResources().getString(R.string.year4), getResources().getString(R.string.price4), getResources().getString(R.string.description4), R.raw.mini1, images));
 
 
         RecyclerView rec = view.findViewById(R.id.recycle);
