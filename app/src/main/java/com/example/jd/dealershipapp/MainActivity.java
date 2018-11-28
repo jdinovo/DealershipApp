deb47b4c96a67eeb4022b6bc8d7a49df9e0772ec
Chris Dias <christopher.dias01@stclairconnect.ca>
2018-11-28 18:44:58 -0500

    Added Material Design -Created a home page with different intents -Added company colours throughout the app -Added appropriate layouts for all screen sizes and layouts in the home page -exernalized strings and dimensions

--- app/src/main/java/com/example/jd/dealershipapp/MainActivity.java
+++ app/src/main/java/com/example/jd/dealershipapp/MainActivity.java
@@ -1,15 +1,12 @@
 package com.example.jd.dealershipapp;
 
-import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -18,8 +15,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import com.balysv.materialmenu.MaterialMenuDrawable;
 
-import com.example.jd.dealershipapp.JavaBean.Vehicle;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,
@@ -35,12 +33,21 @@ public class MainActivity extends AppCompatActivity
 
     FragmentManager fm;
 
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
 
         //Set up the first time we run the application
         fm = getSupportFragmentManager();
@@ -49,22 +56,6 @@ public class MainActivity extends AppCompatActivity
             transaction.replace(R.id.content, new MainFragment(), "main");
             transaction.commit();
         }
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -112,7 +103,18 @@ public class MainActivity extends AppCompatActivity
         FragmentTransaction transaction = fm.beginTransaction();
         //transaction.setCustomAnimations(Insert 2 or 4 animations here);
 
-        if (id == R.id.nav_viewInv) {
+        if (id == R.id.nav_homepage) {
+            Fragment selectedFragment = fm.findFragmentByTag("homepage");
+
+            if(selectedFragment == null) {
+                transaction.replace(R.id.content, new MainFragment(), "homepage");
+                transaction.addToBackStack(null);
+            } else if(!selectedFragment.isVisible()) {
+                transaction.replace(R.id.content, selectedFragment);
+                transaction.addToBackStack(null);
+            }
+
+        } else if (id == R.id.nav_viewInv) {
             Fragment selectedFragment = fm.findFragmentByTag("inv");
 
             if(selectedFragment == null) {
@@ -143,8 +145,6 @@ public class MainActivity extends AppCompatActivity
                 transaction.replace(R.id.content, selectedFragment);
                 transaction.addToBackStack(null);
             }
-        } else if (id == R.id.nav_visitUs) {
-
         } else if (id == R.id.nav_credits) {
             Fragment selectedFragment = fm.findFragmentByTag("credit");
 
